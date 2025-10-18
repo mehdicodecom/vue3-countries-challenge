f73ef1c131928bb77864304a9310c02f55ab5487
Mehdi Rafiei <info@mehdicode.com>
2025-10-18 23:57:05 +0330

    feat: config colors + using tailwind v4 - latest version + testing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <h1 class="text-5xl font-bold">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-neutral-50 dark:bg-primary-900 text-neutral-900 dark:text-neutral-0">
+    <!-- Header -->
+    <header class="bg-neutral-0 dark:bg-primary shadow-md">
+      <div class="max-w-desktop mx-auto px-6 py-6">
+        <h1 class="text-2xl font-extrabold text-neutral-900 dark:text-neutral-0">Where in the world?</h1>
+      </div>
+    </header>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+}
+
+export default config
